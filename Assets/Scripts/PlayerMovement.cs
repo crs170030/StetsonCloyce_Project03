3abd1a6ba55caf898779e3c5c84a97969623929d
Cloyce Stetson <crs170030@utdallas.edu>
2020-11-22 17:45:22 -0600

    Character, World, and Bomb
    Imported free unity asset for a fantasy character. Need to delete unused assets once the project is more finalized. The player doesn't move the bomb. Next on this list is UI

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public LevelController _levelController;
+
+    public CharacterController controller;
+
+    public float speed = 12f;
+    public float run = 1.5f;
+    public float jumpHeight = 3f;
+    public float gravity = -9.81f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    public bool pmMenuIsOpen = false;
+
+    Vector3 velocity;
+    bool isGrounded;
+    float runMultiplyer = 1f;
+
+    //public AudioClip jumpSound;
+    //public AudioSource _playerSounds;
+
+    void Update()
+    {
+        if (!pmMenuIsOpen)
+        {
+            //check if player is on the ground
+            isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+            if (isGrounded && velocity.y < 0)
+            {
+                velocity.y = -2f;
+            }
+
+            float x = Input.GetAxis("Horizontal");
+            float z = Input.GetAxis("Vertical");
+
+            //Running
+            if (Input.GetKey(KeyCode.LeftShift) && isGrounded)
+            {
+                runMultiplyer = run;
+                //_levelController.SprintFOV(true);
+                //Debug.Log("Gotta go fast! Run Multiplyer == " + runMultiplyer);
+            }
+            else if (!Input.GetKey(KeyCode.LeftShift))
+            {
+                runMultiplyer = 1f;
+                //_levelController.SprintFOV(false);
+            }
+
+            //move character with a and d
+            Vector3 move = ((transform.right * x) + (transform.forward * z));
+
+            //move with respect to speed and time
+            controller.Move(move * (speed * runMultiplyer) * Time.deltaTime);
+
+            //jump
+            if (Input.GetButtonDown("Jump") && isGrounded)
+            {
+                velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+                //_playerSounds.PlayOneShot(jumpSound, 1f);
+            }
+
+            //set velocity to gravity
+            velocity.y += gravity * Time.deltaTime;
+
+            //move with velocity with time squared
+            controller.Move(velocity * Time.deltaTime);
+        }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        //_levelController.TakeDamage(damage);
+    }
+
+    //public void 
+}
---
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public Transform cam;
+
+    public float speed = 6f;
+    public float turnSmoothTime = 0.1f;
+    public float jumpHeight = 3f;
+    public float gravity = -9.81f;
+
+    float turnSmoothVelocity;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+    public bool isGrounded;
+    //float runMultiplyer = 1f;
+    float angle;
+    float targetAngle;
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float horizontal = Input.GetAxisRaw("Horizontal");  //between -1 and 1
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if(direction.magnitude >= 0.1f)
+        {
+            targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y; //atan = angle between x and y
+            angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+        }
+
+        //jump
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            //_playerSounds.PlayOneShot(jumpSound, 1f);
+        }
+
+        //reset camera if player presses button (R?)
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            //TODO: Add this feature sometime
+        }
+
+        //set velocity to gravity
+        velocity.y += gravity * Time.deltaTime;
+
+        //move with velocity with time squared
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
